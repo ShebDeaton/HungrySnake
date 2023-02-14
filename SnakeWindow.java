44dc19b762fd1021cb935f5f2b49a36e2b65fef7
Hafsah Khan <hafsahtkhan@gmail.com>
2023-02-14 00:48:21 -0600

    Update SnakeWindow.java

--- SnakeWindow.java
+++ SnakeWindow.java
@@ -7,15 +7,33 @@ public class SnakeWindow extends JFrame{
     private int SnakeWindowHeight = 1000;
     JPanel eastPan, snakePan;
 
+    JPanel main;
+    GridBagConstraints c;
+
+
     public SnakeWindow() {
         Snake trialSnake = new Snake();
         this.setTitle("Hungry Snake Project");
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        this.setLayout(new GridLayout(1,2));
-        this.add(trialSnake);
-        this.setSize(SnakeWindowWidth,SnakeWindowHeight);
+
+        //start of testing GridBag Layout
+        main = new JPanel();
+        main.setLayout(new GridBagLayout());
+        c = new GridBagConstraints();
+        c.insets = new Insets(2,2,2,2);
+        c.gridx = 0;
+        c.gridy = 0;
+        c.ipadx = 700;
+        c.ipady = 700;
+        main.add(trialSnake, c);
+        //this.setLayout(new GridLayout(1,2));
+        //this.add(trialSnake);
+        
         windowMenus();
         initOptions();
+
+        this.getContentPane().add(main);
+        this.setSize(SnakeWindowWidth,SnakeWindowHeight);
         //initSnake();
         this.setVisible(true);
     }
@@ -49,11 +67,14 @@ public class SnakeWindow extends JFrame{
     public void initOptions() {
         eastPan = new JPanel();
         eastPan.setBackground(Color.blue);
-        this.add(eastPan);
+        c.gridx = 1;
+        c.ipadx = 20;
+        c.ipady = 20;
 
         JCheckBox speedBox = new JCheckBox("Speed");
 
         eastPan.add(speedBox);
+        main.add(eastPan, c);
     }
 
     public static void main(String[] args) {
