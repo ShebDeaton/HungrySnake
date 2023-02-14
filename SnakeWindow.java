16da64c0f766d67f7540a1b8a65d120d9b1f7bdb
Hafsah Khan <hafsahtkhan@gmail.com>
2023-02-14 00:04:29 -0600

    Update SnakeWindow.java

--- SnakeWindow.java
+++ SnakeWindow.java
@@ -5,17 +5,29 @@ import java.awt.*;
 public class SnakeWindow extends JFrame{
     private int SnakeWindowWidth = 1000;
     private int SnakeWindowHeight = 1000;
+    JPanel eastPan, snakePan;
 
     public SnakeWindow() {
-        Snake trialSnake = new Snake();
+        //Snake trialSnake = new Snake();
         this.setTitle("Hungry Snake Project");
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        this.add(trialSnake);
+        //this.add(trialSnake, BorderLayout.WEST);
         this.setSize(SnakeWindowWidth,SnakeWindowHeight);
+        this.setLayout(new BorderLayout());
         windowMenus();
+        initOptions();
+        initSnake();
         this.setVisible(true);
     }
     
+    public void initSnake() {
+        snakePan = new JPanel();
+        snakePan.setSize(700,700);
+        Snake trialSnake = new Snake();
+        snakePan.add(trialSnake);
+        this.add(snakePan, BorderLayout.WEST);
+    }
+
     public void windowMenus()
     {
         JMenuBar menuBar = new JMenuBar();
@@ -32,6 +44,15 @@ public class SnakeWindow extends JFrame{
                }
             });
         this.setJMenuBar(menuBar);   
+    }
+
+    public void initOptions() {
+        eastPan = new JPanel();
+        this.add(eastPan, BorderLayout.EAST);
+
+        JCheckBox speedBox = new JCheckBox("Speed");
+
+        eastPan.add(speedBox);
     }
 
     public static void main(String[] args) {
